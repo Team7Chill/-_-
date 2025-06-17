ae1b865d6acd0b5b40a50db73aed1d9bcaf977e9
hyunmin <khmin052@naver.com>
2025-06-17 21:11:49 +0900

    fix: 코드 pull 이후 저장

--- src/main/java/com/example/outsourcing_project/domain/task/domain/repository/TaskRepository.java
+++ src/main/java/com/example/outsourcing_project/domain/task/domain/repository/TaskRepository.java
@@ -8,21 +8,54 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.example.outsourcing_project.domain.dashboard.controller.dto.TodayTasksResponseDto;
+
+
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Query("select COUNT(t) from Task t where t.isDeleted = false and t.creator = :userId")
+    long countAll(@Param("userId") Long userId);
+
 
     // 태스크 전체조회 페이징 조회
     Page<Task> findAllByIsDeletedFalse(Pageable pageable); // 삭제되지 않은 태스크만 페이징
 
     // 태스크 검색(제목, 내용, 상태 필터링)
     @Query("SELECT t From Task t " +
-    "WHERE t.isDeleted = false " +
-    "AND (:title IS NULL OR LOWER(t.title) LIKE LOWER(CONCAT('%', :title, '%'))) " +
-    "AND (:content IS NULL OR LOWER(t.content) LIKE LOWER(CONCAT('%', :content, '%'))) " +
-    "AND (:status IS NULL OR t.status = :status)")
+            "WHERE t.isDeleted = false " +
+            "AND (:title IS NULL OR LOWER(t.title) LIKE LOWER(CONCAT('%', :title, '%'))) " +
+            "AND (:content IS NULL OR LOWER(t.content) LIKE LOWER(CONCAT('%', :content, '%'))) " +
+            "AND (:status IS NULL OR t.status = :status)")
     Page<Task> searchTasks(
             @Param("title") String title,
             @Param("content") String content,
-            @Param("status")TaskStatus status,
+            @Param("status") TaskStatus status,
             Pageable pageable
-            );
+    );
+
+    @Query("select COUNT(t) from Task t where t.status = :status and t.isDeleted = false and t.creator = :userId")
+    long countByStatus(@Param("status") TaskStatus status, @Param("userId") Long userId);
+
+    @Query("select COUNT(t) from Task t " +
+            "where t.status in ('TODO', 'IN_PROGRESS') " +
+            "and t.deadLine < current_timestamp " +
+            "and t.isDeleted = false " +
+            "and t.creator = :userId")
+    long countOverdueTasks(@Param("userId") Long userId);
+
+    @Query("select new com.example.outsourcing_project.domain.dashboard.controller.dto.TodayTasksResponseDto(" +
+            "t.id, t.title, t.priority, t.status, t.deadLine) " +
+            "from Task t " +
+            "where t.manager.id = :userId " +
+            "and function('date', t.deadLine) = current_date " +
+            "and t.status in ('TODO', 'IN_PROGRESS') " +
+            "and t.isDeleted = false " +
+            "order by " +
+            "case t.priority " +
+            "when 'HIGH' then 1 " +
+            "when 'MEDIUM' then 2 " +
+            "when 'LOW' then 3 " +
+            "else 4 end")
+    List<TodayTasksResponseDto> findTodayTasks(@Param("userId") Long userId);
 }
