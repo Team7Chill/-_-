8a7565bcf3874123434997f544186bff25b3f628
An-jihyun <anjh322@gmail.com>
2025-06-18 20:00:50 +0900

    feat: 기본 어노테이션 작성

---
+++ src/main/java/com/example/outsourcing_project/domain/user/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.example.outsourcing_project.domain.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/auth")
+public class UserController {
+}
