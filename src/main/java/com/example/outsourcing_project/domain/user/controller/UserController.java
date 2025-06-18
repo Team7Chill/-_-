7a965f25c51a7d72b69114b4bc8e0b9d9308b5bb
An-jihyun <anjh322@gmail.com>
2025-06-18 20:02:23 +0900

    feat: 생성자 주입을 위한 설정 추가

--- src/main/java/com/example/outsourcing_project/domain/user/controller/UserController.java
+++ src/main/java/com/example/outsourcing_project/domain/user/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.example.outsourcing_project.domain.user.controller;
 
+import com.example.outsourcing_project.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class UserController {
+    private final UserService userService;
 }
