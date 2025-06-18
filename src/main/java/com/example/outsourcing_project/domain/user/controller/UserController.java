c54bb1328a3a971613840ed2455ee74d63d9a832
An-jihyun <anjh322@gmail.com>
2025-06-18 20:07:31 +0900

    feat: 회원가입 메서드 추가

--- src/main/java/com/example/outsourcing_project/domain/user/controller/UserController.java
+++ src/main/java/com/example/outsourcing_project/domain/user/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.example.outsourcing_project.domain.user.controller;
 
+import com.example.outsourcing_project.domain.user.controller.dto.RegisterRequestDto;
 import com.example.outsourcing_project.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class UserController {
     private final UserService userService;
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterRequestDto requestDto) {
+        userService.register(requestDto);
+        return ResponseEntity.ok("회원가입 성공");
+    }
 }
