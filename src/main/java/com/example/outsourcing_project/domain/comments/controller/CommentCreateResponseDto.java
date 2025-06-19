6a012b6108d879fa5c186539d39af2b2f8977b8f
Zyooon <wcw7373@gmail.com>
2025-06-19 21:48:08 +0900

    fix : 댓글 생성 시 id 값도 반환되도록 수정

--- src/main/java/com/example/outsourcing_project/domain/comments/controller/CommentCreateResponseDto.java
+++ src/main/java/com/example/outsourcing_project/domain/comments/controller/CommentCreateResponseDto.java
@@ -1,8 +1,6 @@
 package com.example.outsourcing_project.domain.comments.controller;
 
 import com.example.outsourcing_project.domain.comments.model.entity.Comments;
-import com.example.outsourcing_project.domain.task.domain.model.Task;
-import com.example.outsourcing_project.domain.user.domain.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,8 +21,10 @@ public class CommentCreateResponseDto {
     private LocalDateTime createdAt;
 
     public CommentCreateResponseDto(Comments comment) {
+        this.id = comment.getId();
         this.content = comment.getContent();
         this.taskId = comment.getTask().getId();
         this.userId = comment.getUser().getId();
+        this.createdAt = comment.getCreatedAt();
     }
 }
